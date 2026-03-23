798c5a84f7746c311a9ee948336fc2294c72d98f
andy-xu2 <andyrzxu@gmail.com>
2026-03-23 15:58:22 -0400

    displayCard, nextBtn, prevBtn

--- scripts.js
+++ scripts.js
@@ -12,10 +12,39 @@ let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
+    const cardContent = document.getElementById('card-content');
 
+    if (showingTerm) {
+        cardContent.textContent = flashcards[currentIndex].term;
+    } else {
+        cardContent.textContent = flashcards[currentIndex].definition;
+    }
 }
 
 // The rest of the code you will write is apart of event listeners
+const nextButton = document.getElementById('next-btn');
+const prevButton = document.getElementById('prev-btn');
+
+nextButton.addEventListener('click', () => {
+    currentIndex++;
+
+    if (currentIndex >= flashcards.length) {
+        currentIndex = 0;
+    }
+
+    showingTerm = true;
+    displayCard();
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = flashcards.length - 1;
+    }
+    showingTerm = true;
+    displayCard();
+});
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
